b61ce759ecac40966b6d762d6c68ee5d2f31c2b0
shiny <57299745+shinyish@users.noreply.github.com>
2021-04-01 17:54:02 -0700

    Create Comments.lua

---
+++ LuaTutorial/Comments.lua
@@ -0,0 +1,5 @@
+"Comments are a feature in lua that allow you to create input for other developers or yourself to read for reference"
+"This is a very feature for yourself or for development teams."
+
+"Here is an example of a comment in lua"
+-- This is a comment!
